a3b5e67f6b0dfd9a2d95e4020eca7c4cca15677d
djeff971 <kyuside@gmail.com>
2014-11-23 13:30:46 +0100

    Base du projet final

--- Awt/src/test/FirstTest.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class FirstTest {
-
-	public void fuck(){
-		System.out.println("just want to geek" +
-				"bitch");
-	}
-}
---
+++ src/menu/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author djeffreypierre
+ *
+ */
+package menu;
